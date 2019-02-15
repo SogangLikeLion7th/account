5329a62fb0b1887ba1e810cf604083dd3bcba47a
tronze <0183535663@naver.com>
2019-02-15 14:21:15 +0900

    Invitation link update to automatically create uri when sending email

--- admin.py
+++ admin.py
@@ -55,7 +55,7 @@ class UserAdmin(BaseUserAdmin):
             users = User.objects.filter(is_staff=False)
             for user in users:
                 link = RegisterLink.objects.create(user=user)
-                user.email_user("서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요.", "안녕하세요, %s님!\n멋쟁이 사자처럼에 오신걸 진심으로 환영합니다!\n\n1시간 안에 아래 링크를 통해 서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요:\n%s\n\n감사합니다." % (user.name, request.get_host() + reverse('register', kwargs={'uuid': link.uuid.hex})), settings.DEFAULT_FROM_EMAIL)
+                user.email_user("서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요.", "안녕하세요, %s님!\n멋쟁이 사자처럼에 오신걸 진심으로 환영합니다!\n\n1시간 안에 아래 링크를 통해 서강대학교 멋쟁이 사자처럼 사이트에 가입해주세요:\n%s\n\n감사합니다." % (user.name, request.build_absolute_uri(reverse('register', kwargs={'uuid': link.uuid.hex}))), settings.DEFAULT_FROM_EMAIL)
             return redirect('admin:account_user_changelist')
 
 
